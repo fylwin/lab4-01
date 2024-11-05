4c31e3d27199d2069891ee5e603ed2b19410e2ab
fylwin <fyl18872995008@outlook.com>
2024-11-05 21:55:07 +0800

    Update EventService.ts

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/uptoclouds/331-intro-to-vite',
+  baseURL: 'https://my-json-server.typicode.com/fylwin/lab2',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
